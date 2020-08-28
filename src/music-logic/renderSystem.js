99f8a0c5d9a8adde6b497f8d9b8b41dce5ab3f74
Samboozle <samuel.a.bates@gmail.com>
2020-08-27 17:18:53 -0700

    renderSystem.js is still WIP

--- src/music-logic/renderSystem.js
+++ src/music-logic/renderSystem.js
@@ -1,8 +1,11 @@
 import Vex from 'vexflow';
-import _, { property } from 'lodash';
+import _ from 'lodash';
 import theKeys from './theKeys';
 
-const { Accidental, Beam, Factory, Formatter, Registry, Stave, StaveNote, System, StaveTie, Voice } = Vex.Flow;
+const { Accidental, Barline, Beam, Factory, Formatter, Registry, Stave, StaveNote, System, StaveTie, Voice } = Vex.Flow;
+// const { SINGLE, DOUBLE, END, NONE,
+//   REPEAT_BEGIN, REPEAT_BOTH, REPEAT_END
+// } = Barline.type;
 
 // this function was inspired by the BachTests in the VexFlow project on github,
 // specifically: https://github.com/0xfe/vexflow/blob/master/tests/bach_tests.js
@@ -13,8 +16,8 @@ const { Accidental, Beam, Factory, Formatter, Registry, Stave, StaveNote, System
 export default (noodle, windowWidth, windowHeight) => {
 
   // windowSpec
-  let canvasWidth = Math.max(windowWidth * 0.75, 500);
-  let canvasHeight = Math.max(windowHeight * 0.75, 500);
+  let scoreWidth = Math.max(windowWidth * 0.9, 500);
+  // let canvasHeight = Math.max(windowHeight * 0.75, 500);
 
   // Boilerplate
   // registry allows notes to be marked with unique identifiers for later modification,
@@ -29,56 +32,80 @@ export default (noodle, windowWidth, windowHeight) => {
 
   // Helpers
   // shorthand for score.[method]
-  let voice = score.voice.bind(score);
-  let notes = score.notes.bind(score);
-  let beam = score.beam.bind(score);
-  let tuplet = score.tuplet.bind(score);
+  let makeVoice = score.voice.bind(score);
+  let makeNotes = score.notes.bind(score);
+  // let beam = score.beam.bind(score);
+  // let tuplet = score.tuplet.bind(score);
 
   // prefer to give each measure x, y, and width values in reducer below
   // initialize score with first system indented, per engraving custom
   // let x = 120;
   // let y = 20;
 
-  let makeSystem = width => {
+  let makeSystem = (x, y, width) => {
     return vex.System({ x: x, y: y, width: width, spaceBetweenStaves: 10 });
   }
-
-
 
   // todo -> write reducer that determines width and position of current measure
   // by comparing it to density of NEXT measure;
   
-  let measures = noodle.measures.reduce(simplifyMeasures);
+  let measures = noodle.measures.reduce(simplifyMeasures(scoreWidth));
 
   let modifications = {};
 
-
   measures.forEach((measure, mNo) => {
-    let { pickup, timeSig, keySig, staves, /* x, y, width */ } = measure;
-    // let system = makeSystem(width);
+    let { isPickup, keySig, timeSig,
+      showClef, showKey, showTime, 
+      staves, barlines, connectors,
+      /* x, y, */ width
+    } = measure;
 
-    for (let clef in staves) {
-      let stave = staves[clef];
+    let system = makeSystem(width);
+
+    for (let staveClef in staves) {
+      let voices = staves[staveClef];
       let vs = [];
 
-      stave.forEach((voice, vNo) => {
+      voices.forEach((voice, vNo) => {
         let ns = [];
 
         voice.forEach((noteObj, nNo) => {
-          let { keys, duration, modifiers } = noteObj;
+          let { clef, keys, duration, modifiers } = noteObj;
           let note = (keys.length === 1 ? keys[0] : `(${keys.join(" ")})`) + `/${duration}`;
-          if (modifiers) { 
-            let noteId = `M${mNo}C${clef}V${vNo}N${nNo}`;
+          if (modifiers) {
+            // todo -> figure out if each note in keys array gets its own id
+            // the nesting of this measure object lets us assign a unique identifier to each note
+            let noteId = `M${mNo}C${staveClef}V${vNo}N${nNo}`;
             modifications[noteId] = modifiers;
             note += `[id="${noteId}"]`;
           }
-          ns.push(notes(note));
-        })
+          ns.push(makeNotes(note, { clef: clef }));
+        });
 
-        vs.push(ns.reduce(concat));
+        ns = ns.reduce(_.concat);
+        vs.push(makeVoice(ns));
       })
 
-      System.addStave({ voices: vs })
+      let staff = system.addStave({ voices: vs });
+
+      if (showClef) { staff.addClef(staveClef); }
+
+      if (endClef) { staff.addEndClef(endClef); }
+
+      if (showKey) { }
+
+      if (endKey) { }
+
+      if (showTime) { }
+
+      if (endTime) { }
+
+
+      if (connectors.length > 0) {
+        connectors.forEach(conn => { system.addConnector(conn); });
+      } else if (barlines.length > 0) {
+        barlines.forEach
+      }
 
 
 
@@ -98,27 +125,61 @@ export default (noodle, windowWidth, windowHeight) => {
   // AS WELL AS TEMPO/TEMPO CHANGE INFORMATION
 
   // ideally, it will also intelligently identify groups of notes to be beamed and groups of notes to be tupled
-  const simplifyMeasures = (acc, measure) => {
-    let { voices } = measure;
-    
-    if (_.isEmpty(acc)) {
-      measure.timeChange = true;
-      measure.x = 120;
-      measure.y = 20;
-
-      measure.width = Math.min(
-        Math.ceil(beats)
-        ) 
-        return [measure];
-      }
-      
-      return [...acc, measure];
-      
-    }
-    
-  const concat = (a, b) => a.concat(b);
-  const isAccidental = (pitch, key) => !theKeys[key].includes(pitch);
   
 }
 
+// curried to make scoreWidth available
+const simplifyMeasures = scoreWidth = (acc, measure, _index) => {
+  
+  
+  
+  let { staves, barlines, keySig, timeSig, isPickup } = measure;
+  let voices = [];
+  for (let stave of staves) {
+    for (let voice of stave) {
+      voices.push(voice);
+    }
+  }
+  
+  let maxLength = _.maxBy(voices, v => v.length);
+  
+  measure.connectors = [];
+  
+  // conditions for first measure
+  if (_.isEmpty(acc)) {
+    measure.showClef = true;
+    measure.showKey = true;
+    measure.showTime = true;
+    measure.x = 80;
+    measure.y = 20;
+    measure.width = Math.min(
+      maxLength * 30,
+      scoreWidth - measure.x - 20
+    );
+
+    if (Object.keys(staves).length > 1) {
+      measure.connectors.push("brace");
+    }
+
+    return [measure];
+  }
+  
+  // subsequent measures
+  let prev = _.initial(acc);
+  let lastMeasure  = _.last(acc);
+  if (_.isEmpty(prev)) {
+
+  } else {
+    
+  }
+
+
+
+
+  return [...acc, measure];
+
+}
+
+// const concat = (a, b) => a.concat(b);
+const isAccidental = (pitch, key) => !theKeys[key].includes(pitch);
 
--- src/passages/minuetOne.js
+++ src/passages/minuetOne.js
@@ -1,25 +1,30 @@
-import { isPlainObject } from "lodash";
+import Vex from 'vexflow';
 
 export default {
   measures: [
-    { pickup: null,
+    { isPickup: false,
       timeSig: "3/4",
       keySig: "G",
+      // clefs: ["treble", "bass"],
       staves: {
         "treble": [
-          [ { keys: ["d5"], duration: "q", modifiers: null },
-            { keys: ["g4"], duration: "8", modifiers: null },
-            { keys: ["a4"], duration: "8", modifiers: null },
-            { keys: ["b4"], duration: "8", modifiers: null },
-            { keys: ["c5"], duration: "8", modifiers: null },
+          [ { clef: "treble", keys: ["d5"], duration: "q", modifiers: null },
+            { clef: "treble", keys: ["g4"], duration: "8", modifiers: null },
+            { clef: "treble", keys: ["a4"], duration: "8", modifiers: null },
+            { clef: "treble", keys: ["b4"], duration: "8", modifiers: null },
+            { clef: "treble", keys: ["c5"], duration: "8", modifiers: null },
           ]
         ],
         "bass": [
-          [ { keys: ["g3", "b3", "d4"], duration: "h", modifiers: null },
-            { keys: [] }
+          [ { clef: "bass", keys: ["g3", "b3", "d4"], duration: "h", modifiers: null },
+            { clef: "bass", keys: ["a3"], duration: "q", modifiers: null }
           ]
         ]
-      }
+      },
+    //   barlines: {
+    //     left: "none",
+    //     right: "single"
+    //   }
     }
   ],
   // clefs: ["treble", "bass"]
